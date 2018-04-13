a7bedeaec622622269c7da5310a55a471f6fab75
JPy <jparis.py@gmail.com>
2018-04-13 09:21:53 +0200

    Removing all files specified in .gitignore

--- openscraper/base_handler.py
+++ openscraper/base_handler.py
@@ -217,9 +217,10 @@ class BaseHandler(tornado.web.RequestHandler):
 
 
 	def set_default_headers(self, *args, **kwargs):
-
-		app_log.info("setting headers")
-		
+		""" 
+		set some handlers to be able to respond to AJAX GET queries
+		"""
+		# app_log.info("setting headers")
 		self.set_header("Access-Control-Allow-Origin", "*")
 		self.set_header("Access-Control-Allow-Headers", "x-requested-with")
 		self.set_header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
